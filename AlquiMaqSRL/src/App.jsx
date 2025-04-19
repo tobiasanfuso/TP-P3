5eab596c21199aefbc4e75b480add97d771c2e74
Lautaro Ceballos <lautaceba18@gmail.com>
2025-04-19 12:20:24 -0300

    commit main into lauchabranch

--- AlquiMaqSRL/src/App.jsx
+++ AlquiMaqSRL/src/App.jsx
@@ -2,28 +2,18 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import './App.css'
-<<<<<<< Updated upstream
-=======
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import FormLogin from './components/auth/FormLogin'
 import MainScreen from './components/dashboard/MainSceen'
 
 
->>>>>>> Stashed changes
 
 const ProtectedRoute = ({ user, element }) => {
   return user !== null ? element : <Navigate to="/login" />;
 };
 
 function App() {
-<<<<<<< Updated upstream
-  
-
-  return (
-    <h1>ALQUIMAQ</h1>
-  )
-=======
   const [user, setUser] = useState(null); // Estado inicial del usuario
 
   return (
@@ -43,7 +33,6 @@ function App() {
       </Routes>
     </Router>
   );
->>>>>>> Stashed changes
 }
 
 export default App
