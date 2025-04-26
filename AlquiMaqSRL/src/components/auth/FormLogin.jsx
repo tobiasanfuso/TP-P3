8bb9a9f7aaf35c21f3035cb11d85e3098e777c5e
Lautaro Ceballos <lautaceba18@gmail.com>
2025-04-26 20:26:11 -0300

    mejora de inicio de sesion y rutas protegidas, agregado de pagina not found

--- AlquiMaqSRL/src/components/auth/FormLogin.jsx
+++ AlquiMaqSRL/src/components/auth/FormLogin.jsx
@@ -1,76 +1,107 @@
 import React from 'react';
+import { Card, Form, Button, Row, Col, FormGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import './FormLogin.css';
 
 
 
-const FormLogin = ({ setUser }) => {
-  const [usuario, setUsuario] = useState("")
+const FormLogin = ({ setUser, onLogin }) => {
+  const [userName, setUserName] = useState("")
   const [password, setPassword] = useState("")
-  const [error, setError] = useState(false)
   const navigate = useNavigate();
+  const userRef = useRef(null)
+  const passwordRef = useRef(null)
+
+  const [errors, setErrors] = useState({
+    userName:false,
+    password:false
+  })
+
+
+  const handleUserChange = (e) => {
+    setUserName(e.target.value)
+    setErrors({ ...errors, userName: false })
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+    setErrors({ ...errors, password: false })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if (usuario === "" || password === "") {
-      setError(true)
+
+    //logica de validacion
+
+    if (!userRef.current.value.length) {
+      setErrors({ ...errors, userName: true })
+      userRef.current.focus()
       return
     }
-    setError(false)
+    else if (!passwordRef.current.value.length || password.length < 7) {
+      setErrors({ ...errors, password: true })
+      passwordRef.current.focus()
+      return
+    }
+    setErrors({userName: false, password: false})
     
+    //logica de roles
     let role = "customer";
-    if (usuario.toLowerCase() === "admin") {
+    if (userName.toLowerCase() === "admin") {
       role = "admin";
-    } else if (usuario.toLowerCase() === "sysadmin"){
+    } else if (userName.toLowerCase() === "sysadmin") {
       role = "sysadmin";
     }
-    
-    setUser({name:usuario, role});
-    
+
+    setUser({ name: userName, role });
+    onLogin();
     navigate("/main");
   }
 
 
-  return (
-    <div className='bg-dark vh-100 vw-100'>
-      <div className='text-center py-4'>
-        <h1 className='text-light'>AlquiMaq S.R.L🛠️</h1>
-      </div>
-      <div className="d-flex justify-content-center align-items-center bg-dark vh-100 vw-100">
-        <div className="card bg-light text-dark p-4 shadow-lg rounded border border-secondary"
-          style={{ width: '100%', maxWidth: '700px' }}>
-          <h2 className="text-center mb-4">Iniciar Sesión</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">Usuario</label>
-              <input
-                type="text"
-                className="form-control"
-                value={usuario}
-                onChange={(e) => setUsuario(e.target.value)}
-                required
+      return (
+  <Card className="mt-5 mx-3 p-3 px-5 shadow form-card display-flex flex-column align-items-center">
+      <Card.Body>
+        <Row className="mb-2">
+          <h5>¡Bienvenidos a AlquiMaq S.R.L!</h5>
+        </Row>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup className="mb-4">
+            <Form.Control
+              type="username"
+              required
+              className={errors.userName && "border border-danger"}
+              ref={userRef}
+              placeholder="Ingresar usuario"
+              onChange={handleUserChange}
+              value={userName} />
+              <p className="text-danger mt-2">{errors.userName && "El campo usuario es obligatorio"}</p>
+          </FormGroup>
+          <FormGroup className="mb-4">
+            <Form.Control
+            className={errors.password && "border border-danger"}
+              type="password"
+              required
+              ref={passwordRef}
+              placeholder="Ingresar contraseña"
+              onChange={handlePasswordChange}
+              value={password}
               />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Contraseña</label>
-              <input
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            {error && <div className="text-danger mb-3">Por favor completa todos los campos.</div>}
-            <button type="submit" className="btn btn-primary w-100">
-              Ingresar
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+              <p className="text-danger mt-2">{errors.password && "La contraseña es incorrecta"}</p>
+          </FormGroup>
+          <Row>
+            <Col />
+            <Col md={6} className="d-flex justify-content-end">
+              <Button variant="secondary" type="submit">
+                Iniciar sesión
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Card.Body>
+    </Card>
+              
   )
 }
 
