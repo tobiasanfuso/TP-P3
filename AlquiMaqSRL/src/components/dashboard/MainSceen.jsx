c2aeb51f9a4bb28900abaa2bc540681b72373414
tobiasanfuso <tobianfuso@gmail.com>
2025-05-13 19:43:44 -0300

    modal + datalles

--- AlquiMaqSRL/src/components/dashboard/MainSceen.jsx
+++ AlquiMaqSRL/src/components/dashboard/MainSceen.jsx
@@ -5,17 +5,22 @@ import { useNavigate } from 'react-router-dom'
 import NewProduct from '../NewProduct/NewProduct'
 import ProductCard from '../ProductCard/ProductCard'
 import './MainScreen.css'
+import ProductModal from '../ProductModal/ProductModal';
 
 const MainScreen = ({ user, setUser,logOut }) => {
   const navigate = useNavigate();
 
   const [products, setProducts] = useState([
-    { id: 1, title: "Agricola 1", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://placeholder.pics/svg/400x250/FFDEAA-FFA2A2" },
-    { id: 2, title: "Agricola 2", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://placeholder.pics/svg/400x250/FFDEAA-FFA2A2" },
-    { id: 3, title: "Agricola 3", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://placeholder.pics/svg/400x250/FFDEAA-FFA2A2" },
+    { id: 1, title: "Martillo", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://imgs.search.brave.com/AnxguX9a4sEPITLiMWj7O5hPBn4xZmXXrr0eJtgGu68/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jbXMu/Z3J1cG9mZXJyZXBh/dC5uZXQvYXNzZXRz/L2ltZy9wcm9kdWN0/b3MvSE0xODEyXzEu/d2VicA" },
+    { id: 2, title: "Vibropisonador", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://imgs.search.brave.com/pI6_3UPtgqKnZ4CVHZ3owowSbPeY9-jszT3o-EqoNzU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZGlyZWN0aW5kdXN0/cnkuZXMvaW1hZ2Vz/X2RpL3Bob3RvLW1n/LzQxMTU2LTE3ODE1/OTI3LmpwZw" },
+    { id: 3, title: "Amoladora", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://imgs.search.brave.com/LnLVvUVGB7EyvEbCt-mhiN2d4C9i8Y3dncmrbnaOi0g/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kMjho/aTkzZ3I2OTdvbC5j/bG91ZGZyb250Lm5l/dC8xYWIzMjc0NS0y/NjBiLWNjODQvaW1n/L1Byb2R1Y3RvLzI4/Lzk1NjRQQ1YtMS02/Mjk5M2IyZDg2Yzkx/LmpwZWc" },
+    {id: 4, title: "Cuerpo de Andamio", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://imgs.search.brave.com/S6ZIVdexpcnq_ohiKz09c92FHv5nc3uMd-nPLEmvhG4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hY2Ru/LXVzLm1pdGllbmRh/bnViZS5jb20vc3Rv/cmVzLzk1Mi85MzYv/cHJvZHVjdHMvYW5k/YW1pby1yZWZvcnph/ZG9vbzEtMmE3ODc3/Y2RmNzYzNmM0NWQx/MTU2MDg5NTYwMzA4/NzItMjQwLTAuanBn" },
+    {id: 5, title: "Allanadora", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", image: "https://imgs.search.brave.com/YIpzjqePgFcQFLGqIPKmCm6Rs4Y1zF7MOLBmyeMBkkc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9iYXJp/a2VsbHN1ZGFtZXJp/Y2EuY29tLmFyL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE5LzEx/L0FsbGFuYWRvcmFf/c2ltcGxlX0JBUklL/RUxMX2RpYW1ldHJv/MTIwY21fY2FqYUhl/YXZ5RHV0eTJfY2hp/Y28ucG5n" },
   ])
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
 
   const currentRole = user.role;
   const currentUser = user.name;
@@ -38,6 +43,10 @@ const MainScreen = ({ user, setUser,logOut }) => {
     setProducts([...products, productWhithId]);
   }
 
+  const handleselectproduct = () => {
+    const 
+
+  }
 
   //menu segun el rol
   const navItems = [
@@ -123,7 +132,7 @@ const MainScreen = ({ user, setUser,logOut }) => {
                 title={product.title}
                 description={product.description}
                 image={product.image}
-                onDetails={() => alert(`Detalles de ${product.title}`)}
+                onDetails={() => setSelectedProduct(product)}
               />
             </div>
           )
@@ -131,6 +140,11 @@ const MainScreen = ({ user, setUser,logOut }) => {
           }
         </div>
       </main>
+      <ProductModal
+  product={selectedProduct}
+  onClose={() => setSelectedProduct(null)}
+/>
+
       <footer className="text-center p-3 bg-secondary text-light vw-100">
         <p>© 2025 AlquiMaq S.R.L. Todos los derechos reservados.</p>
       </footer>
