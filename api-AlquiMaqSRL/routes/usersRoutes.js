d7c98d1a3dd85d1d147ec20c73f02d8dd26a1dc5
tobiasanfuso <tobianfuso@gmail.com>
2025-06-25 12:56:17 -0300

    panel sysadmin

--- api-AlquiMaqSRL/routes/usersRoutes.js
+++ api-AlquiMaqSRL/routes/usersRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
 import { checkRole } from "../middleware/roleMiddleware.js";
 import {getAllUsers,getUserById,updateUser,deleteUser} from "../controllers/usersController.js";
+import { getAllUsers, getUserById, updateUser, deleteUser, createUser } from "../controllers/usersController.js";
+
 
 const router = express.Router();
 
@@ -11,5 +13,7 @@ router.get('/', getAllUsers);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
+router.post('/', createUser); 
+router.post('/', register);
 
 export default router;
